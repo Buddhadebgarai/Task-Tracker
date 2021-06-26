b48893e110e54626f3d7ab844e56f982af064d6a
Buddhadeb Garai <buddhadebgarai2016@gmail.com>
2021-06-26 10:04:05 +0530

    Adding my code

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+const About = () => {
+    return (
+        <div>
+            <center>
+           <h4> 
+               Add Your Task .
+          
+            </h4> 
+           <Link to="/"> Go Back</Link>
+           </center>
+
+        </div>
+    )
+} 
+
+export default About
